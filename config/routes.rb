dde29e00cb8e3e66110091ad7cd95bacf50ed0f3
arnaud cormier <arno.cormier@gmail.com>
2016-01-23 15:06:42 +0100

    views & controllers for project & task

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
 
+
+
   ActiveAdmin.routes(self)
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get 'pages/about'
 
-  root 'pages#about'
+  root 'project#index'
+
+  resources :project do
+    resources :task, only: [:show]
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
